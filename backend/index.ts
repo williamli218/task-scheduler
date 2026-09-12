d920dbab86eebf8af4dc3705b7240d3752bea7e1
William Li <william.jy.li@gmail.com>
2026-09-12 18:27:32 -0400

    Prepare for deployment

--- backend/index.ts
+++ backend/index.ts
@@ -7,7 +7,7 @@ import eventRoutes from "./routes/events";
 import categoryRoutes from "./routes/categories";
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173" }));
 app.use(express.json());
 
 app.use("/auth", authRoutes);
